7fdfcce18105c297d8305990513aca66aeeeebec
Phani_Kiran <pret321@gmail.com>
2019-07-30 15:54:52 -0500

    Finds the places with space as well

--- WebViewKit/ViewController.swift
+++ WebViewKit/ViewController.swift
@@ -14,13 +14,14 @@ class ViewController: UIViewController {
     @IBOutlet weak var textField: UITextField!
     
     @IBAction func buttonPressed(_ sender: Any) {
-        let newtextField = textField.text
-        let newString = newtextField!.replacingOccurrences(of: " ", with: "_")
-        let weather = "https://www.weather-forecast.com/locations/" + newString + "/forecasts/latest"
+        let newtextField = textField.text!
+        let newString = newtextField.replacingOccurrences(of: " ", with: "-")
+        
         let startWord = "p class=\"b-forecast__table-description-content\">"
-        let url = URL(string: weather)
+        let url = URL(string: "https://www.weather-forecast.com/locations/" + newString + "/forecasts/latest")!
+        print(url)
         do {
-            let contents = try String(contentsOf: url!)
+            let contents = try String(contentsOf: url)
             var newcontents=contents.components(separatedBy: "<")
             var newIndex = newcontents.firstIndex(of: startWord)
             var newword = newcontents[newIndex!+1]
